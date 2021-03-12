b55e9f64d215148127e98bd3084dfd5d2f674e39
PorridgePi <kunbao2006@126.com>
2021-03-12 22:57:49 +0800

    D01: Add files

---
+++ D01-D15-Introduction/D01.playground/Contents.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+/*
+ Day 1 - variables, simple data types, and string interpolation
+ */
+
+// 2. Strings and integers
+var str = "Hello, playground" // string
+
+var age = 38 // integer
+
+var population = 8_000_000 // integer with separator
+
+
+// 3. Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+""" // multi-line string
+
+var str2 = "This also goes\nover multiple lines" // multi-line string
+
+var str3 = """
+This does not \
+go over multiple \
+lines
+""" // to format code neatly
+
+
+// 4. Doubles and Booleans
+var pi = 3.14159265 // double
+
+var awesome = true // boolean
+
+
+// 5. String interpolation
+var str4 = "The value of pi is approximately \(pi)" // string interpolation (插值)
+
+
+// 6. Constants
+let constant = "this is a constant value" // use "let" for constants
+
+
+// 7. Type annotations
+let typeInt: Int = 1
+let typeString: String = "String"
+let typeNotSingle: Double = 1.1
+let typeBOOOOOOOLEAN: Bool = false
