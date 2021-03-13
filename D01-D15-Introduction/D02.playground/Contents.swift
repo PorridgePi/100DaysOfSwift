6f3a498f4688ded9dadb96f4337c5b4b20ee7864
PorridgePi <kunbao2006@126.com>
2021-03-13 23:58:47 +0800

    D02: feat: All done

--- D01-D15-Introduction/D02.playground/Contents.swift
+++ D01-D15-Introduction/D02.playground/Contents.swift
@@ -42,3 +42,100 @@ let keys = Set(["A001", "A002", "A003"])
 // Tuples - specific, fixed collection of related values, with each value having a precise location or name
 let address = (block: 381, street: "Clementi Avenue 5", postal: 120381)
 
+
+// 5. Dictionaries
+// When using type annotations, use this: [String: Double], [String: String] etc.
+
+let heights = [
+    "John": 1.65,
+    "Sean": 1.71
+]
+heights["John"]
+
+
+// 6. Dictionary default values
+// If value for key does not exist, Swift will return nil.
+
+let favouriteIceCream = [
+    "John": "Chocolate"
+]
+favouriteIceCream["Paul"] // will return nil
+favouriteIceCream["Paul", default: "Unknown"]
+
+
+// 7. Creating empty collections
+// Collections include all of the above
+
+var teams = [String: String]()
+teams["Paul"] = "Red"
+
+var results = [Int]()
+
+var words = Set<String>()
+var numbers = Set<Int>()
+
+// Alternatively for arrays and dictionaries:
+
+var teams1 = Dictionary<String, String>()
+var results1 = Array<Int>()
+
+
+// 8. Enumerations
+// a.k.a. enums - a way of defining groups of related values in a way that makes them easier to use
+
+let result = "failure"
+let result2 = "failed"
+let result3 = "fail"
+
+enum Result {
+    case success
+    case failure
+}
+
+// Stops us from accidentally using different strings each time
+
+let result4 = Result.failure
+
+
+// 9. Enum associated values
+// Makes it more nuanced
+
+enum Activity {
+    case bored
+    case running(destination: String)
+    case talking(topic: String)
+    case singing(volume: Int)
+}
+
+let talking = Activity.talking(topic: "Swift") // can be more precise now
+
+
+// 10. Enum raw values
+
+enum Planet: Int {
+    case mercury
+    case venus
+    case earth
+    case mars
+}
+
+let earth = Planet(rawValue: 2) // numerical order
+
+enum Planet2: Int {
+    case mercury = 1
+    case venus
+    case earth
+    case mars
+} // Swift will assign mecury to 1, and then count upwards from there
+
+let earth2 = Planet2(rawValue: 3)
+
+
+// 11. Summary
+// complex data types - all of the data types above store a group of items under a single value
+// array - stores items in order added - access via numerical positions
+// set - stores items in no order - cannot access via numerical positions
+// tuple - fixed in size, name can be assigned to items - items can be accessed via numerical positions or names
+// dictionary - stores item according to a key - items can be read using the key
+// enums - a way of grouping related values so we can use it without mistakes
+// a raw value can be attached to enums, and associated values can be added to store additional information about each case
