ede6a1fbc7beb8506db3475df82264969c7939c3
PorridgePi <kunbao2006@126.com>
2021-03-13 13:47:03 +0800

    D02: feat: Arrays vs sets vs tuples

--- D01-D15-Introduction/D02.playground/Contents.swift
+++ D01-D15-Introduction/D02.playground/Contents.swift
@@ -32,3 +32,13 @@ name = ("Steve", "Jobs") // values can be changed, but not the type (e.g. last:1
 name.0
 name.first
 
+
+// 4. Arrays vs sets vs tuples
+
+// Arrays - duplicates allowed, order matters
+let tallest = ["John", "Gabriel", "Eric", "Terry", "John"]
+// Sets - unique values, need to check for presence of value quickly
+let keys = Set(["A001", "A002", "A003"])
+// Tuples - specific, fixed collection of related values, with each value having a precise location or name
+let address = (block: 381, street: "Clementi Avenue 5", postal: 120381)
+
