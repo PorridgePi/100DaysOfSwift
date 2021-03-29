4865af5efd6189cfa8b4fbd34ba554243803d922
PorridgePi <kunbao2006@126.com>
2021-03-29 22:18:10 +0800

    D08: Add files

---
+++ D01-D15-Introduction/D08.playground/Contents.swift
@@ -0,0 +1,54 @@
+import UIKit
+
+/*
+ Day 8 - structs, part one
+ */
+
+// 1. Creating structs
+struct Sport {
+    var name: String
+}
+
+var tennis = Sport(name: "Tennis")
+print(tennis.name)
+
+tennis.name = "Lawn tennis"
+
+
+// 2. Computed properties
+struct Sport2 {
+    var name: String
+    var isCCA: Bool
+    
+    var CCAStatus: String {
+        if isCCA {
+            return "\(name) is a CCA"
+        } else {
+            return "\(name) is not a CCA"
+        }
+    }
+}
+
+let football = Sport2(name: "football", isCCA: false)
+print(football.CCAStatus)
+
+
+// 3. Property observers
+// Let you run code before or after any property changes
+
+struct Progress {
+    var task: String
+    var amount: Int {
+        didSet { // Use didSet property observer to run code when amount changes
+            print("\(task) is now \(amount)% complete")
+        }
+    }
+}
+
+// Use willSet to take action before a property changes, but that is rarely used.
+
+var progress = Progress(task: "Compiling program", amount: 0)
+progress.amount = 30
+progress.amount = 80
+progress.amount = 100
+
