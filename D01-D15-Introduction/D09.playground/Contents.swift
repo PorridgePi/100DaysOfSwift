4a63ef997ea346a6d50f7600633c067e185fa0d8
PorridgePi <kunbao2006@126.com>
2021-04-01 13:52:05 +0800

    D09: feat: Add 3 and 4

--- D01-D15-Introduction/D09.playground/Contents.swift
+++ D01-D15-Introduction/D09.playground/Contents.swift
@@ -32,3 +32,66 @@ struct User2 {
 
 var user2 = User2("twoset")
 
+
+// 3. Lazy properties
+
+struct FamilyTree {
+    init() {
+        print("Creating family tree!")
+    }
+}
+
+struct Person {
+    var name: String
+    var familyTree = FamilyTree()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var ed = Person(name: "Ed")
+// familyTree is created immediately
+
+// What if it is not always needed?
+// Use lazy var - will only be created when it is first accessed
+
+struct Person2 {
+    var name: String
+    lazy var familyTree = FamilyTree()
+    
+    init(name: String) {
+        self.name = name
+    }
+}
+
+var john = Person2(name: "John")
+// familyTree not immediately created
+
+print("This is a line break.")
+
+john.familyTree
+// familyTree is now created since it is accessed
+
+
+// 4. Static properties and methods
+// Ask Swift to share specific properties and methods across all instances of the struct
+// By declaring them as static
+
+struct Student {
+    static var classSize = 0
+    var name: String
+
+    init(name: String) {
+        self.name = name
+        Student.classSize += 1
+    }
+}
+
+var charles = Student(name: "Charles")
+print(Student.classSize)
+// CANNOT be accessed by charles.classSize
+// because classSize belongs to struct itself, not instances of the struct
+
+var joe = Student(name: "Joe")
+print(Student.classSize)
