447f29821a5881e62631a889a6d2361121a84cb2
PorridgePi <kunbao2006@126.com>
2021-04-01 12:56:24 +0800

    D08: feat: All done

--- D01-D15-Introduction/D08.playground/Contents.swift
+++ D01-D15-Introduction/D08.playground/Contents.swift
@@ -82,3 +82,36 @@ var city2 = City2(population: 12, gdp: 12.0)
 print(city2.calcGDPPerCapita())
 city2.increaseGDP()
 print(city2.calcGDPPerCapita())
+
+
+// 6. Properties and methods of strings
+
+let string = "Do or do not, there is no try."
+
+print(string.count) // count number of characters
+print(string.hasPrefix("Do")) // true if string starts with specific letters
+print(string.uppercased()) // UPPERCASE
+print(string.sorted()) // sort the letters of string into array
+
+
+// 7. Properties and methods of arrays
+
+var toys = ["Woody"]
+
+print(toys.count) // number of items
+toys.append("Buzz") // add new item
+toys.firstIndex(of: "Buzz") // locate item in array
+print(toys.sorted()) // sort itmes of array
+toys.remove(at: 0) // remove an item
+
+
+// 8. Structs part one summary
+/*
+ 1. Create structs
+ 2. Computed properties in structs
+ 3. Property observers - runs when property changes
+ 4. Methods - func
+ 5. Properties and methods of strings
+ 6. Properties and methods of arrays
+ */
+
